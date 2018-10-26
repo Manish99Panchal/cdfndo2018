d920d5e4e8fdd411b35f801a8d81dd21d3a6ec37
Manish99Panchal <30552881+Manish99Panchal@users.noreply.github.com>
2018-10-26 23:50:49 +0530

    Add files via upload

---
+++ rcode.R
@@ -0,0 +1,15 @@
+library(RCurl)
+url = getURL("http://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv")
+forest_fires <- read.csv(text = url)
+head(forest_fires)
+# get a model matrix
+ff_clean = model.matrix(~.,  data=forest_fires)
+ff_clean = as.data.frame(ff_clean)
+# write the csv file to my coursera folder
+library(readr)
+write_csv(x=ff_clean, path="E:/forestfire/datafile/forest_fires.csv")
+# get column print out for python (line 19)
+library(pystr)
+cols = colnames(ff_clean)
+dput(pystr_upper(cols))
+
